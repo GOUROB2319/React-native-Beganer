b51d8d50a1b441787a64f0583ea77f3b1508b5e9
Gourob Saha <gourobsaha2319@gmail.com>
2025-08-21 19:51:58 +0600

    dfg

--- React-native-Beganer/src/components/icons/SearchIcon.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react";
-import type { SVGProps } from "react";
-const Component = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17.5 17.5" {...props}><path xmlns="http://www.w3.org/2000/svg" stroke="#000" strokeLinecap="round" strokeWidth={1.5} d="m16.75 16.75-3.78-3.784m2.1-5.058a7.17 7.17 0 0 1-2.1 5.061 7.15 7.15 0 0 1-5.06 2.097c-1.9 0-3.72-.754-5.06-2.097a7.13 7.13 0 0 1-2.1-5.061c0-1.898.75-3.719 2.1-5.062A7.15 7.15 0 0 1 7.91.75c1.9 0 3.72.754 5.06 2.096a7.17 7.17 0 0 1 2.1 5.062z" /></svg>;
-export default Component;
